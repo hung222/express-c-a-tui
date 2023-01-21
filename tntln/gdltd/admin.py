225c6568d76186f72960b922ef3d6ed7406b3b89
jojok <jimmy@gmail.com>
2023-01-21 17:14:50 +0700

    upload thu muc tntln vo github

---
+++ tntln/gdltd/admin.py
@@ -0,0 +1,3 @@
+from django.contrib import admin
+from. models import gdltd
+admin.site.register(gdltd)
---
+++ tntln/gdltd/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GdltdConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'gdltd'
