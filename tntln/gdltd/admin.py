54b978478bafd2358bcad6dabb4d40499e337155
hung222 <90371118+hung222@users.noreply.github.com>
2023-01-24 07:54:06 +0700

    Delete tntln directory

--- tntln/gdltd/admin.py
+++
@@ -1,3 +0,0 @@
-from django.contrib import admin
-from. models import gdltd
-admin.site.register(gdltd)
--- tntln/gdltd/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-class gdltd(models.Model):
-       tencnv = models.CharField(max_length=800)
-       nghenghiep = models.CharField(max_length=1200)
-       tinhcach = models.CharField(max_length=1500)
